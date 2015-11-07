8e7d8cfa161982444278a819903875c7affab496
Johnny Chan <jc2johnny@gmail.com>
2015-11-07 17:17:27 -0500

    form flow completed

---
+++ app/helpers/articles_helper.rb
@@ -0,0 +1,5 @@
+module ArticlesHelper
+    def article_params
+        params.require(:article).permit(:title, :body, :rating, :address, :zipcode, :city, :state, :sport, :players, :tag_list, :image  )
+    end
+end
