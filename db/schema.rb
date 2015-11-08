b761e84b1116f0cb567c2f061a0d25ee08fbb649
Johnny Chan <jc2johnny@gmail.com>
2015-11-07 19:37:47 -0500

    added images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151108000522) do
+ActiveRecord::Schema.define(version: 20151108001915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,9 +26,13 @@ ActiveRecord::Schema.define(version: 20151108000522) do
     t.string   "state"
     t.string   "sport"
     t.integer  "players"
-    t.text     "player_names", default: [], array: true
+    t.text     "player_names",       default: [], array: true
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
